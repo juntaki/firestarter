120a3922a56c37d8bc6523f6c0bc51071a7cc80c
juntaki <me@juntaki.com>
2018-03-03 19:09:53 +0900

    Add swagger UI

--- generate.sh
+++ generate.sh
@@ -2,6 +2,7 @@
 
 # go get github.com/twitchtv/twirp/protoc-gen-twirp
 # go get github.com/thechriswalker/protoc-gen-twirp_js
+# go get github.com/elliots/protoc-gen-twirp_swagger
 
 protoc \
     -I proto \
@@ -9,5 +10,6 @@ protoc \
     --twirp_out=proto \
     --go_out=proto \
     --twirp_js_out=./admin/proto \
+    --twirp_swagger_out=swagger-ui \
     --js_out=import_style=commonjs,binary:./admin/proto \
     proto/*.proto
